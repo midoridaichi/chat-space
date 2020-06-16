57c8534782aa5b47b85f44adac6fc303372ae245
midoridaichi <win_trinita@yahoo.co.jp>
2020-06-16 20:19:07 +0900

    ChatSpaceコメント機能の実装

---
+++ app/controllers/edits_controller.rb
@@ -0,0 +1,6 @@
+def index
+end
+
+def update
+  =link to "http://localhost:3000/groups/12/messages"
+end
---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,17 @@
+class ImageUploader < CarrierWave::Uploader::Base
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  include CarrierWave::MiniMagick
+
+  # Choose what kind of storage to use for this uploader:
+  storage :file
+  # storage :fog
+
+# 省略
+
+  # Create different versions of your uploaded files:
+  # version :thumb do
+    process resize_to_fit: [800, 800]
+  # end
+
+# 省略
